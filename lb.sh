f7addbe44d7604b78d89e4648431da0af67acd2a
skommana04 <skommana04@gmail.com>
2026-01-05 13:45:15 -0600

    added checks

---
+++ lb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#pwd=/home/ec2-user/3-tier-expense_shellscripts
+project_adirectory=$(pwd)
+a=$(id -u)
+if [ $a -eq 0 ]
+then
+    echo -e " $G you are a root user $N"
+else
+    echo -e "you are not a root user $N"
+    exit 1
+fi
+
+validate_check() 
+{
+    if [ $1 -eq 0 ]
+    then
+        echo -e " $G $2 successful $N"
+    else
+        echo -e " $R $2 failed $N"
+    fi
+}
+
+
+dnf install nginx -y 
+validate_check $? "nginx installation"
+
+systemctl enable nginx
+validate_check $? "nginx enable"
+
+
+systemctl start nginx
+validate_check $? "nginx start"
+
+cp $project_adirectory/expense-lb.conf /etc/nginx/conf.d/expense-lb.conf
+validate_check $? "nginx lb configuration copy"
+
+systemctl restart nginx
+validate_check $? "nginx restart"
+
