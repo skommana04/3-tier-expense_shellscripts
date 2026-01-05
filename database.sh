098cfc6a7fd7e6ded35efff4d618b4db80d0e7d8
skommana04 <skommana04@gmail.com>
2026-01-05 07:06:23 -0600

    added shellscripts

---
+++ database.sh
@@ -0,0 +1,29 @@
+#!/bin/bash/
+
+validate_check()
+{
+if [ $1 -ne 0 ]
+then
+    echo "$2 failed"
+    exit 1
+else
+    echo "$2 successful"
+fi
+}
+
+
+#mysql server installation
+dnf install mysql-server -y
+vaidate_check $? "mysql installation"
+
+#start mysql server
+systemctl start mysqld
+validate_check $? "mysql start"
+
+#enable mysql server
+systemctl enable mysqld
+validate_check $? "mysql enable"
+
+#secure mysql installation
+mysql_secure_installation --set-root-pass ExpenseApp@1
+validate_check $? "mysql secure installation"   
